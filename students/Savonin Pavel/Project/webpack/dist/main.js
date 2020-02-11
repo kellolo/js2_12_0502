d1de379365711ef916aa5f9789d718931e6a6a5f
pahapuha <pahapuha@gmail.com>
2020-02-11 17:09:05 +0300

    22

--- students/Savonin Pavel/Project/webpack/dist/main.js
+++ students/Savonin Pavel/Project/webpack/dist/main.js
@@ -1,1 +1,134 @@
-!function(e){var t={};function r(n){if(t[n])return t[n].exports;var o=t[n]={i:n,l:!1,exports:{}};return e[n].call(o.exports,o,o.exports,r),o.l=!0,o.exports}r.m=e,r.c=t,r.d=function(e,t,n){r.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:n})},r.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},r.t=function(e,t){if(1&t&&(e=r(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var n=Object.create(null);if(r.r(n),Object.defineProperty(n,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)r.d(n,o,function(t){return e[t]}.bind(null,o));return n},r.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return r.d(t,"a",t),t},r.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},r.p="",r(r.s=0)}([function(e,t){}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/index.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _public_style_style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./public/style/style.css */ \"./src/public/style/style.css\");\n/* harmony import */ var _public_style_style_css__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_public_style_style_css__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _public_style_normalize_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./public/style/normalize.css */ \"./src/public/style/normalize.css\");\n/* harmony import */ var _public_style_normalize_css__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_public_style_normalize_css__WEBPACK_IMPORTED_MODULE_1__);\n/* harmony import */ var _public_js_main_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./public/js/main.js */ \"./src/public/js/main.js\");\n/* harmony import */ var _public_js_main_js__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_public_js_main_js__WEBPACK_IMPORTED_MODULE_2__);\n\r\n\r\n\r\n\r\n\r\n\r\n\n\n//# sourceURL=webpack:///./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/public/js/main.js":
+/*!*******************************!*\
+  !*** ./src/public/js/main.js ***!
+  \*******************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("//заглушки (имитация базы данных)\r\nconst image = 'https://placehold.it/200x150';\r\nconst cartImage = 'https://placehold.it/100x80';\r\nconst items = ['Notebook', 'Display', 'Keyboard', 'Mouse', 'Phones', 'Router', 'USB-camera', 'Gamepad'];\r\nconst prices = [1000, 200, 20, 10, 25, 30, 18, 24];\r\nconst ids = [1, 2, 3, 4, 5, 6, 7, 8];\r\n\r\n\r\n//глобальные сущности корзины и каталога (ИМИТАЦИЯ! НЕЛЬЗЯ ТАК ДЕЛАТЬ!)\r\n// var userCart = [];\r\nvar list\r\n\r\n//кнопка скрытия и показа корзины\r\n// document.querySelector('.btn-cart').addEventListener('click', () => {\r\n//     document.querySelector('.cart-block').classList.toggle('invisible');\r\n// });\r\n//кнопки удаления товара (добавляется один раз)\r\n// document.querySelector('.cart-block').addEventListener ('click', (evt) => {\r\n//     if (evt.target.classList.contains ('del-btn')) {\r\n//         removeProduct (evt.target);\r\n//     }\r\n// })\r\n//кнопки покупки товара (добавляется один раз)\r\n// document.querySelector('.products').addEventListener ('click', (evt) => {\r\n//     if (evt.target.classList.contains ('buy-btn')) {\r\n//         addProduct (evt.target);\r\n//     }\r\n// })\r\n\r\n//создание массива объектов - имитация загрузки данных с сервера\r\nfunction fetchData() {\r\n    let arr = [];\r\n    for (let i = 0; i < items.length; i++) {\r\n        arr.push(createProduct(i));\r\n    }\r\n    return arr\r\n};\r\n\r\n//создание товара\r\nfunction createProduct(i) {\r\n    return {\r\n        id: ids[i],\r\n        name: items[i],\r\n        price: prices[i],\r\n        img: image,\r\n        quantity: 0,\r\n        createTemplate: function () {\r\n            return `<div class=\"product-item\" data-id=\"${this.id}\">\r\n                        <img src=\"${this.img}\" alt=\"Some img\">\r\n                        <div class=\"desc\">\r\n                            <h3>${this.name}</h3>\r\n                            <p>${this.price} $</p>\r\n                            <button class=\"buy-btn\" \r\n                            data-id=\"${this.id}\"\r\n                            data-name=\"${this.name}\"\r\n                            data-image=\"${this.img}\"\r\n                            data-price=\"${this.price}\">Купить</button>\r\n                        </div>\r\n                    </div>`\r\n        },\r\n\r\n        add: function () {\r\n            this.quantity++\r\n        }\r\n    }\r\n};\r\n\r\n//рендер списка товаров (каталога)\r\nfunction renderProducts() {\r\n    let arr = [];\r\n    for (item of list) {\r\n        arr.push(item.createTemplate())\r\n    }\r\n    document.querySelector('.products').innerHTML = arr.join();\r\n}\r\n\r\n\r\n\r\n\r\nfunction init() {\r\n    console.log('init start')\r\n    list = fetchData();\r\n    renderProducts();\r\n}\r\n\r\ninit()\r\n\r\n//CART\r\n\r\n// Добавление продуктов в корзину\r\n// function addProduct (product) {\r\n//     let productId = +product.dataset['id'];\r\n//     let find = userCart.find (element => element.id === productId);\r\n//     if (!find) {\r\n//         userCart.push ({\r\n//             name: product.dataset ['name'],\r\n//             id: productId,\r\n//             img: cartImage,\r\n//             price: +product.dataset['price'],\r\n//             quantity: 1\r\n//         })\r\n//     }  else {\r\n//         find.quantity++\r\n//     }\r\n//     renderCart ()\r\n// }\r\n\r\n// //удаление товаров\r\n// function removeProduct (product) {\r\n//     let productId = +product.dataset['id'];\r\n//     let find = userCart.find (element => element.id === productId);\r\n//     if (find.quantity > 1) {\r\n//         find.quantity--;\r\n//     } else {\r\n//         userCart.splice(userCart.indexOf(find), 1);\r\n//         document.querySelector(`.cart-item[data-id=\"${productId}\"]`).remove()\r\n//     }\r\n//     renderCart ();\r\n// }\r\n\r\n// //перерендер корзины\r\n// function renderCart () {\r\n//     let allProducts = '';\r\n//     for (el of userCart) {\r\n//         allProducts += `<div class=\"cart-item\" data-id=\"${el.id}\">\r\n//                             <div class=\"product-bio\">\r\n//                                 <img src=\"${el.img}\" alt=\"Some image\">\r\n//                                 <div class=\"product-desc\">\r\n//                                     <p class=\"product-title\">${el.name}</p>\r\n//                                     <p class=\"product-quantity\">Quantity: ${el.quantity}</p>\r\n//                                     <p class=\"product-single-price\">$${el.price} each</p>\r\n//                                 </div>\r\n//                             </div>\r\n//                             <div class=\"right-block\">\r\n//                                 <p class=\"product-price\">${el.quantity * el.price}</p>\r\n//                                 <button class=\"del-btn\" data-id=\"${el.id}\">&times;</button>\r\n//                             </div>\r\n//                         </div>`\r\n//     }\r\n\r\n//     document.querySelector(`.cart-block`).innerHTML = allProducts;\r\n// }\r\nclass GoodsItem {\r\n    constructor(title, price) {\r\n        this.title = title;\r\n        this.price = price;\r\n    }\r\n    render() {\r\n        return `<div class=\"goods-item\"><h3>${this.title}</h3><p>${this.price}</p></div>`;\r\n    }\r\n}\r\n\r\nclass GoodsList {\r\n    constructor() {\r\n        this.goods = [];\r\n    }\r\n    fetchGoods() {\r\n        this.goods = [{\r\n                title: 'Shirt',\r\n                price: 150\r\n            },\r\n            {\r\n                title: 'Socks',\r\n                price: 50\r\n            },\r\n            {\r\n                title: 'Jacket',\r\n                price: 350\r\n            },\r\n            {\r\n                title: 'Shoes',\r\n                price: 250\r\n            },\r\n        ];\r\n    }\r\n\r\n    render() {\r\n        let listHtml = '';\r\n        this.goods.forEach(good => {\r\n            const goodItem = new GoodsItem(good.title, good.price);\r\n            listHtml += goodItem.render();\r\n        });\r\n        document.querySelector('.goods-list').innerHTML = listHtml;\r\n    }\r\n\r\n    totalPrice() {\r\n        let total = 0;\r\n        this.goods.forEach(good => {\r\n            total += +good.price\r\n        })\r\n    }\r\n}\r\n\r\nconst goods = [{\r\n        title: 'Shirt',\r\n        price: 150\r\n    },\r\n    {\r\n        title: 'Socks',\r\n        price: 50\r\n    },\r\n    {\r\n        title: 'Jacket',\r\n        price: 350\r\n    },\r\n    {\r\n        title: 'Shoes',\r\n        price: 250\r\n    },\r\n]\r\n\r\nclass Catalog {\r\n    constructor(container) {\r\n        this.items = []\r\n        this.container = container //'#app'\r\n        this._init()\r\n    }\r\n\r\n    _init() {\r\n        this.items = goods\r\n        this._render()\r\n    }\r\n\r\n    _render() {\r\n        let str = ''\r\n        this.items.forEach(el => {\r\n            str += `<div class=\"goods-item\"><h3>${el.title}</h3><p>${el.price}</p></div>`\r\n        })\r\n        document.querySelector(this.container).innerHTML = str\r\n    }\r\n}\n\n//# sourceURL=webpack:///./src/public/js/main.js?");
+
+/***/ }),
+
+/***/ "./src/public/style/normalize.css":
+/*!****************************************!*\
+  !*** ./src/public/style/normalize.css ***!
+  \****************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./src/public/style/normalize.css?");
+
+/***/ }),
+
+/***/ "./src/public/style/style.css":
+/*!************************************!*\
+  !*** ./src/public/style/style.css ***!
+  \************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./src/public/style/style.css?");
+
+/***/ })
+
+/******/ });
--- students/Savonin Pavel/Project/webpack/src/public/js/main.js
+++ students/Savonin Pavel/Project/webpack/src/public/js/main.js
@@ -7,37 +7,37 @@ const ids = [1, 2, 3, 4, 5, 6, 7, 8];
 
 
 //глобальные сущности корзины и каталога (ИМИТАЦИЯ! НЕЛЬЗЯ ТАК ДЕЛАТЬ!)
-var userCart = [];
-var list = fetchData ();
+// var userCart = [];
+var list
 
 //кнопка скрытия и показа корзины
-document.querySelector('.btn-cart').addEventListener('click', () => {
-    document.querySelector('.cart-block').classList.toggle('invisible');
-});
+// document.querySelector('.btn-cart').addEventListener('click', () => {
+//     document.querySelector('.cart-block').classList.toggle('invisible');
+// });
 //кнопки удаления товара (добавляется один раз)
-document.querySelector('.cart-block').addEventListener ('click', (evt) => {
-    if (evt.target.classList.contains ('del-btn')) {
-        removeProduct (evt.target);
-    }
-})
+// document.querySelector('.cart-block').addEventListener ('click', (evt) => {
+//     if (evt.target.classList.contains ('del-btn')) {
+//         removeProduct (evt.target);
+//     }
+// })
 //кнопки покупки товара (добавляется один раз)
-document.querySelector('.products').addEventListener ('click', (evt) => {
-    if (evt.target.classList.contains ('buy-btn')) {
-        addProduct (evt.target);
-    }
-})
+// document.querySelector('.products').addEventListener ('click', (evt) => {
+//     if (evt.target.classList.contains ('buy-btn')) {
+//         addProduct (evt.target);
+//     }
+// })
 
 //создание массива объектов - имитация загрузки данных с сервера
-function fetchData () {
+function fetchData() {
     let arr = [];
     for (let i = 0; i < items.length; i++) {
-        arr.push (createProduct (i));
+        arr.push(createProduct(i));
     }
     return arr
 };
 
 //создание товара
-function createProduct (i) {
+function createProduct(i) {
     return {
         id: ids[i],
         name: items[i],
@@ -59,14 +59,14 @@ function createProduct (i) {
                     </div>`
         },
 
-        add: function() {
+        add: function () {
             this.quantity++
         }
     }
 };
 
 //рендер списка товаров (каталога)
-function renderProducts () {
+function renderProducts() {
     let arr = [];
     for (item of list) {
         arr.push(item.createTemplate())
@@ -74,60 +74,163 @@ function renderProducts () {
     document.querySelector('.products').innerHTML = arr.join();
 }
 
-renderProducts ();
+
+
+
+function init() {
+    console.log('init start')
+    list = fetchData();
+    renderProducts();
+}
+
+init()
 
 //CART
 
 // Добавление продуктов в корзину
-function addProduct (product) {
-    let productId = +product.dataset['id'];
-    let find = userCart.find (element => element.id === productId);
-    if (!find) {
-        userCart.push ({
-            name: product.dataset ['name'],
-            id: productId,
-            img: cartImage,
-            price: +product.dataset['price'],
-            quantity: 1
+// function addProduct (product) {
+//     let productId = +product.dataset['id'];
+//     let find = userCart.find (element => element.id === productId);
+//     if (!find) {
+//         userCart.push ({
+//             name: product.dataset ['name'],
+//             id: productId,
+//             img: cartImage,
+//             price: +product.dataset['price'],
+//             quantity: 1
+//         })
+//     }  else {
+//         find.quantity++
+//     }
+//     renderCart ()
+// }
+
+// //удаление товаров
+// function removeProduct (product) {
+//     let productId = +product.dataset['id'];
+//     let find = userCart.find (element => element.id === productId);
+//     if (find.quantity > 1) {
+//         find.quantity--;
+//     } else {
+//         userCart.splice(userCart.indexOf(find), 1);
+//         document.querySelector(`.cart-item[data-id="${productId}"]`).remove()
+//     }
+//     renderCart ();
+// }
+
+// //перерендер корзины
+// function renderCart () {
+//     let allProducts = '';
+//     for (el of userCart) {
+//         allProducts += `<div class="cart-item" data-id="${el.id}">
+//                             <div class="product-bio">
+//                                 <img src="${el.img}" alt="Some image">
+//                                 <div class="product-desc">
+//                                     <p class="product-title">${el.name}</p>
+//                                     <p class="product-quantity">Quantity: ${el.quantity}</p>
+//                                     <p class="product-single-price">$${el.price} each</p>
+//                                 </div>
+//                             </div>
+//                             <div class="right-block">
+//                                 <p class="product-price">${el.quantity * el.price}</p>
+//                                 <button class="del-btn" data-id="${el.id}">&times;</button>
+//                             </div>
+//                         </div>`
+//     }
+
+//     document.querySelector(`.cart-block`).innerHTML = allProducts;
+// }
+class GoodsItem {
+    constructor(title, price) {
+        this.title = title;
+        this.price = price;
+    }
+    render() {
+        return `<div class="goods-item"><h3>${this.title}</h3><p>${this.price}</p></div>`;
+    }
+}
+
+class GoodsList {
+    constructor() {
+        this.goods = [];
+    }
+    fetchGoods() {
+        this.goods = [{
+                title: 'Shirt',
+                price: 150
+            },
+            {
+                title: 'Socks',
+                price: 50
+            },
+            {
+                title: 'Jacket',
+                price: 350
+            },
+            {
+                title: 'Shoes',
+                price: 250
+            },
+        ];
+    }
+
+    render() {
+        let listHtml = '';
+        this.goods.forEach(good => {
+            const goodItem = new GoodsItem(good.title, good.price);
+            listHtml += goodItem.render();
+        });
+        document.querySelector('.goods-list').innerHTML = listHtml;
+    }
+
+    totalPrice() {
+        let total = 0;
+        this.goods.forEach(good => {
+            total += +good.price
         })
-    }  else {
-        find.quantity++
     }
-    renderCart ()
 }
 
-//удаление товаров
-function removeProduct (product) {
-    let productId = +product.dataset['id'];
-    let find = userCart.find (element => element.id === productId);
-    if (find.quantity > 1) {
-        find.quantity--;
-    } else {
-        userCart.splice(userCart.indexOf(find), 1);
-        document.querySelector(`.cart-item[data-id="${productId}"]`).remove()
+const goods = [{
+        title: 'Shirt',
+        price: 150
+    },
+    {
+        title: 'Socks',
+        price: 50
+    },
+    {
+        title: 'Jacket',
+        price: 350
+    },
+    {
+        title: 'Shoes',
+        price: 250
+    },
+]
+
+class Catalog {
+    constructor(container) {
+        this.items = []
+        this.container = container //'#app'
+        this._init()
     }
-    renderCart ();
+
+    _init() {
+        this.items = goods
+        this._render()
+    }
+
+    _render() {
+        let str = ''
+        this.items.forEach(el => {
+            str += `<div class="goods-item"><h3>${el.title}</h3><p>${el.price}</p></div>`
+        })
+        document.querySelector(this.container).innerHTML = str
+    }
 }
 
-//перерендер корзины
-function renderCart () {
-    let allProducts = '';
-    for (el of userCart) {
-        allProducts += `<div class="cart-item" data-id="${el.id}">
-                            <div class="product-bio">
-                                <img src="${el.img}" alt="Some image">
-                                <div class="product-desc">
-                                    <p class="product-title">${el.name}</p>
-                                    <p class="product-quantity">Quantity: ${el.quantity}</p>
-                                    <p class="product-single-price">$${el.price} each</p>
-                                </div>
-                            </div>
-                            <div class="right-block">
-                                <p class="product-price">${el.quantity * el.price}</p>
-                                <button class="del-btn" data-id="${el.id}">&times;</button>
-                            </div>
-                        </div>`
-    }
-
-    document.querySelector(`.cart-block`).innerHTML = allProducts;
+class Cart {
+    constructor() {}
+    _render()
 }
