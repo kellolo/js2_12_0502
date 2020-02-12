9761aae8c2317cb8ab5721ec3bd2c161d1fb19da
Anastasia Maremshaova <maremshaowa.nasti@mail.ru>
2020-02-12 13:44:09 +0300

    New

---
+++ students/Maremshaova_Anastasia/Project/webpack_dir/webpack.config.js
@@ -0,0 +1,32 @@
+let minCss = require('mini-css-extract-plugin'); 
+let htmlPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    module: {
+        rules:[
+            {
+                test: /\.css$/,
+                use: [{
+                    loader: minCss.loader,
+                    options: {
+              // you can specify a publicPath here
+              // by default it uses publicPath in webpackOptions.output
+              publicPath: '../',
+              hmr: process.env.NODE_ENV === 'production',
+            },}, 
+                'css-loader']
+            }, {}, {}
+        ]
+    },
+    plugins: [
+        new minCss({
+            filename: 'css/[name].css',
+            chunkFilename: '[id].css',
+            ignoreOrder: false, // Enable to remove warnings about conflicting order
+          }),
+          new htmlPlugin({  // Also generate a test.html
+            template: './src/public/index.html'
+          })
+
+    ]
+}
