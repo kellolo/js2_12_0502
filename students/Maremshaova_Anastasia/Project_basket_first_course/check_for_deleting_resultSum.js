d67ba4b718b8877068446867a28e1da1490ba8fe
Edward <47450322+EdwardLeks@users.noreply.github.com>
2020-02-12 20:20:36 +0200

    upload

--- students/Maremshaova_Anastasia/Project_basket_first_course/check_for_deleting_resultSum.js
+++
@@ -1,8 +0,0 @@
-function checkForDeletingResultSum(basket){
-    if (basket.table.rows.length === 1){
-        basket.div_basket.removeChild(document.querySelector('.div_result'));  
-    }
-    else{
-        addResultSumInBasketTable(basket); 
-    }
-};
--- students/Zhalnina Olga/Project/iswebpack/src/index.js
+++
@@ -1,5 +0,0 @@
-import './public/style/style.css'
-import './public/style/normalize.css'
-
-import './public/js/main.js'
-
