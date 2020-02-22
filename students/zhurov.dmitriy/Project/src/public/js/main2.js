ba1a01c63aa029cb2f5168721edd1549d2f82d18
Shaddix <shaddix.jacoby@gmail.com>
2020-02-22 17:35:51 +0700

    first steps to vue

--- students/zhurov.dmitriy/Project/src/public/js/main2.js
+++ students/zhurov.dmitriy/Project/src/public/js/main2.js
@@ -1,50 +1,33 @@
 let app = new Vue({
     el: '#app',
     data: {
+        url: 'https://raw.githubusercontent.com/Shadzen/js2_12_0502/master/students/zhurov.dmitriy/Project/src/server/db/',
         catalog: {
             content: []
         },
         basket: {
-            show: false,
             totalCost: 0,
             totalQty: 0,
             content: []
-        },
-        url: 'https://raw.githubusercontent.com/Shadzen/js2_12_0502/master/students/zhurov.dmitriy/Project/src/server/db/'
-        // testData: 'Hello Vue',
-        // visible: true
-        //данные, создаваемые и изменяемые внутри vue-компонента
+        }
     },
     methods: {
-        //функции, которые изменяют объект или данные data
         getData(url) {
             return fetch(url)
                 .then(d => d.json())
-        },
-        toggle(el) {
-            el.hidden !== undefined ? el.hidden = !el.hidden : this.$set(el, 'hidden', true)
         }
-        // fooMethod () {
-        //     alert (this.testData)
-        // }
     },
-    computed: {
-        // testComputed () {
-        //     return 'Computed Value: ' + this.testData
-        // }
-        //функции, должные возвращать значения
-    },
+    computed: {},
     mounted() {
-        //событие, когда vue-компонент встроился в ДОМ-модель
         this.getData(this.url + 'catalog.json')
-            .then(items => {
-                this.catalog.content = items
+            .then(data => {
+                this.catalog.content = data
                 console.log(this.catalog.content)
             })
         this.getData(this.url + 'basket.json')
-            .then(items => {
-                this.basket.content = items.content
-                console.log(this.basket.content)
+            .then(data => {
+                this.basket = data
+                console.log(this.basket)
             })
     }
 })
