02b52985d65b3900a4cf0eb90cb0cba2fd56804f
Grigoriy mikirtumov <mikigrigorij@yandex.ru>
2020-02-23 19:36:15 +0300

    homework-5

--- students/Grigoriy_Mikirtumov/Project/vue/js/main.js
+++ students/Grigoriy_Mikirtumov/Project/vue/js/main.js
@@ -1,24 +1,42 @@
 let app = new Vue({
     el: '#app',
     data:{
-        testData: 'hello Vue',
-        visible: true,
+        items:[],
+        url:'http://jsonplaceholder.typicode.com/comments'
+        // testData: 'hello Vue',
+        // visible: true,
         //Данные создаваемые и изменяемые внутри vue компонента
     },
     methods:{
-        //Функции которые изменяют обьект или данные data
-        fooMethod(){
-            alert(this.testData)
+        
+        getData(url){
+           return fetch(url).then(d=>d.json())
+        },
+        
+        toggle(el){
+            el.hidden ? el.hidden = !el.hidden : this.$set(el, 'hidden', true)
         }
+        
+
+        
+        //Функции которые изменяют обьект или данные data
+        // fooMethod(){
+        //     alert(this.testData)
+        // }
     },
     computed:{
         //Функции которые должны возвращать значения
-        testComputed(){
-            return "Computed value: "+this.testData;
-        }
+        // testComputed(){
+        //     return "Computed value: "+this.testData;
+        // }
     },
     //Хуки жизненного цикла
-    mounted: {
+    mounted() {
+        this.getData(this.url).then(posts=>{
+            this.items = posts;
+            console.log(this.items)
+        })
+        
         //событие когда VUE компонент встроился в DOM модель
     }
 })
