e582f1174d6f911ec2382706048e13e7487f90b5
AlyonaCh <fresh1210@yandex.ru>
2020-02-25 16:40:23 +0500

    endLasson5

---
+++ students/ChernyaevaAlyona/Project/web_pack/src/public/components/catalog.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="products">
+        <item v-for="item of this.$parent.catalogItems" :key="item.id_product" :prod="item"/>  
+    </div>
+</template>
+
+<script>
+import item from './catalogItem.vue'
+export default {
+    data() {
+        return {
+            // url: '/index.json',
+            // items: [],
+        }
+    },
+    components: {
+        item
+    // },
+    // mounted() {
+    //     this.$parent.getData(this.url)
+    //     .then(data => {this.items = data})
+    //     .then(() => { this.$parent.getQuantity(this.items) })
+    //     //console.log(this)
+    }
+}
+</script>
