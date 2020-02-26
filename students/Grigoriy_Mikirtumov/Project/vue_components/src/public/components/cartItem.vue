ced46d856d19dd4543f948ee18981d0db45865ef
Grigoriy mikirtumov <mikigrigorij@yandex.ru>
2020-02-26 20:17:46 +0300

    homework-6

---
+++ students/Grigoriy_Mikirtumov/Project/vue_components/src/public/components/cartItem.vue
@@ -0,0 +1,31 @@
+<template>
+    <div  class="cart-item" >
+        <div class="product-bio">
+            <img :src="cartImg" :alt="prodCart.product_name">
+            <div class="product-desc">
+                <p class="product-title">{{prodCart.product_name}}</p>
+                <p class="product-quantity">Quantity: {{prodCart.quantity}}</p>
+                <p class="product-single-price">$ {{prodCart.price}} each</p>
+            </div>
+        </div>
+        <div class="right-block">
+            <p class="product-price">{{ prodCart.quantity * prodCart.price }}</p>
+            <button class="del-btn">&times;</button>
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    props:{
+        cartImg:{
+            type: String,
+            default:'https://placehold.it/200x150'
+        },
+        prodCart:{
+            type: Object
+        }
+    }
+}
+</script>
