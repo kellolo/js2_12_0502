47d6846e11d392179c69af41bff6c86ab8b9f1d1
Maja <ignidrake@gmail.com>
2020-02-26 17:32:58 +0100

    6 компоненты

---
+++ students/MajaFaber/Project/webp/src/public/components/cartItem.vue
@@ -0,0 +1,31 @@
+<template>
+                    <div>
+                        <div class="product-bio">
+                            <img :src="cartImg">
+                            <div class="product-desc">
+                                <p class="product-title">{{prod.title}}</p>
+                                <p class="product-quantity">Quantity: {{prod.quantity}}</p>
+                                <p class="product-single-price">$ {{prod.price}} each</p>
+                            </div>
+                        </div>
+                        <div class="right-block">
+                            <p class="product-price">{{ prod.quantity * prod.price }}</p>
+                            <button class="del-btn" name="del-btn"
+                            @click="removeProduct(prod)">&times;</button>
+                        </div>
+                    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    cartImg: {
+      type: String,
+      default: 'https://placehold.it/100x80'
+    }, 
+    prod: {
+      type: Object
+    }
+  }
+}
+</script> 
---
+++ students/MajaFaber/Project/webp/src/public/components/catalog.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="products">
+        <item v-for="item of items" :key="item.id" :prod="item"  />     
+    </div>
+</template>
+
+<script>
+import item from './catalogItem.vue'
+export default {
+    data() {
+        return {
+            url: '/catalogData.json',
+            items: [],
+        }
+    },
+    components: {
+        item
+    },
+    mounted() {
+        this.$parent.getData(this.url)
+        .then(data => {this.items = data})
+    }
+}
+</script>
