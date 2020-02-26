b0f8b63527e9cc7dd8bcee0d446bcb8d9930e4ac
Vladislav <vl241095@gmail.com>
2020-02-26 15:28:28 +0500

    6

---
+++ students/Vladislav Gordin/Others/unit6/src/public/components/cart.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="cart" v-if="visible">
+        <div class="cart-block">
+            <p v-if="items.length === 0">Корзина пуста.</p>
+            <item v-for="item of items" :key="item.product_id" :cart_item="item"/>
+            <div v-if="this.items.length > 0">
+                <hr>
+                Товаров в корзине: {{ quantity }} (стаков: {{ items.length }})<br>
+                Общая стоимость: {{ price }}$
+                <button name="wipe" class="btn" @click="wipe()">Очистить корзину</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
