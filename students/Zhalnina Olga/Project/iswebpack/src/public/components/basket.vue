e47127b0284b828ef42d28d4fe39599005ac82fe
Olga Zh <olgazhaln@gmail.com>
2020-02-26 15:59:33 +0400

    vue for basket

---
+++ students/Zhalnina Olga/Project/iswebpack/src/public/components/basket.vue
@@ -0,0 +1,25 @@
+<template>    
+    <div class="cart-block">
+        <element v-for="item of elements" :key="item.id_product" :cart="item" :basketImg="img" />
+    </div>
+</template>
+
+<script>
+import item from './basketItem.vue'
+export default {
+    data() {
+        return {
+            url: '/basket.json',
+            elements: [],
+            
+        }
+    },
+    components: {
+        item
+    },
+    mounted() {
+        this.$parent.getData(this.url)
+        .then(data => {this.elements = data.contents})        
+    }
+}
+</script>
---
+++ students/Zhalnina Olga/Project/iswebpack/src/public/views/shop.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <header>
+            <div class="logo">E-shop</div>
+            <div class="cart">
+                <form action="#" class="search-form">
+                    <input type="text" class="search-field">
+                    <button class="btn-search" type="submit">
+                        <i class="fas fa-search"></i>
+                    </button>
+                </form>
+                <button class="btn-cart" @click="showBasket = !showBasket" type="button">Корзина</button>
+                <basket />                
+            </div>
+        </header>
+        <main>        
+            <catalog />               
+        </main>
+    </div>
+</template>
+
+<script>
+import catalog from '../components/catalog.vue'
+import basket from '../components/basket.vue'
+
+export default {
+    data() {
+        return {
+            API: 'https://raw.githubusercontent.com/OlgaZh-UX/js2_12_0502/master/students/Zhalnina%20Olga/Project/iswebpack/src/server/db/'
+        }
+    },
+    components: {
+        catalog, // 'catalog': catalog
+        basket
+    },
+    methods:{
+        getData (url) {
+            return fetch(this.API + url).then(d => d.json())                    
+        },
+    }
+}
+</script>
+
+<style>
+
+</style>
