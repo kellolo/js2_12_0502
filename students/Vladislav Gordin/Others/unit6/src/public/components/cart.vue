08adae958c56dfc4296203fc4e219a438bef4db7
Vladislav <vl241095@gmail.com>
2020-02-26 22:32:15 +0500

    unit7

--- students/Vladislav Gordin/Others/unit6/src/public/components/cart.vue
+++ students/Vladislav Gordin/Others/unit6/src/public/components/cart.vue
@@ -1,24 +1,30 @@
 <template>
-    <div id="cart" v-if="visible">
-        <div class="cart-block">
-            <p v-if="items.length === 0">Корзина пуста.</p>
-            <item v-for="item of items" :key="item.product_id" :cart_item="item"/>
-            <div v-if="this.items.length > 0">
-                <hr>
-                Товаров в корзине: {{ quantity }})<br>
-                Общая стоимость: {{ price }}$
-                <button name="wipe" class="btn" @click="wipe()">Очистить корзину</button>
-            </div>
+    <div class="product-item" :key="item.id_product">
+        <img :src="catalogImg" :alt="item.product_name">
+        <div class="desc">
+            <h3>{{item.product_name}}</h3>
+            <p>{{item.price}} $</p>
+            <button class="buy-btn" name="buy-btn" @click="addProduct(item)">Купить</button>
         </div>
     </div>
 </template>
 
 <script>
+import item from './cartItem.vue'
 export default {
-    
+    data() {
+        return {
+            url: '/getBasket.json',
+            items: [],
+        }
+    },
+    components: {
+        item
+    },
+    mounted() {
+        this.$parent.getData(this.url)
+        .then(data => {this.items = data})
+        //console.log(this)
+    }
 }
 </script>
-
-<style>
-
-</style>
