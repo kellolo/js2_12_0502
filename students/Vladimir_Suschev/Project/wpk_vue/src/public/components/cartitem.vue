e620594503d97d7542a63cd64d8fe20d31e659d5
Vladimir Suschev <sushev_vv@list.ru>
2020-02-28 20:51:55 +0300

    serverver comlit изменил порт с 8080 на 8085 тк конфликтует с VLC player, включенная запись лекции сломала всё)))

--- students/Vladimir_Suschev/Project/wpk_vue/src/public/components/cartitem.vue
+++ students/Vladimir_Suschev/Project/wpk_vue/src/public/components/cartitem.vue
@@ -1,30 +1,37 @@
 <template>
-  <div class="cart-item">
-    <div class="product-bio">
-      <img :src="cartImg" :alt="cart.product_name" />
-      <div class="product-desc">
-        <p class="product-title">{{cart.product_name}}</p>
-        <p class="product-quantity">Quantity: {{cart.quantity}}</p>
-        <p class="product-single-price">$ {{cart.price}} each</p>
-      </div>
+    <div class="cart-item" :key="item.id_product">
+        <div class="product-bio">
+            <img :src="cartImg" :alt="item.product_name">
+            <div class="product-desc">
+                <p class="product-title">{{item.product_name}}</p>
+                <p class="product-quantity">Quantity: {{item.quantity}}</p>
+                <p class="product-single-price">$ {{item.price}} each</p>
+            </div>
+        </div>
+        <div class="right-block">
+            <p class="product-price">{{ item.quantity * item.price }}</p>
+            <button class="del-btn" name="del-btn"
+            @click="removeProduct(item)">&times;</button>
+        </div>
     </div>
-    <div class="right-block">
-      <p class="product-price">{{ cart.quantity * cart.price }}</p>
-      <button class="del-btn" name="del-btn" @click="removeProduct(cart)">&times;</button>
-    </div>
-  </div>
 </template>
-<script>
 
+<script>
 export default {
-props: {
+    props: {
         cartImg: {
             type: String,
             default: 'https://placehold.it/100x80'
         },
-        cart: {
+        item: {
             type: Object
         }
-    }
-};
+    },
+    methods: {
+        removeProduct(prod) {
+            // this.$parent.removeProduct(prod)
+            this.$parent.$emit('remove', prod)
+        }
+    },
+}
 </script>
